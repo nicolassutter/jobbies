c0b4f3128753a233f39a8518907f46a79cd4f532
nicolassutter <contact.sutter.nicolas@gmail.com>
2025-01-25 17:15:19 +0100

    chore: wip upgrade to tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({}), react(), tsconfigPaths()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
+  plugins: [TanStackRouterVite({}), react(), tsconfigPaths(), tailwindcss()],
 });
