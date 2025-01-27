8b14fabb29e8aeee284c7bdb6b478f6fcfd2c56c
nicolassutter <contact.sutter.nicolas@gmail.com>
2025-01-27 15:29:23 +0100

    chore: move some files over to backend package

--- packages/backend/server/utils/db.ts
+++ packages/backend/server/utils/db.ts
@@ -1,6 +1,6 @@
 import { drizzle as devDrizzle } from "drizzle-orm/pglite";
 import { drizzle as productionDrizzle } from "drizzle-orm/node-postgres";
-import { env } from "../env";
+import { env } from "./env";
 
 export const db = env.DEV
   ? devDrizzle("./pglite")
