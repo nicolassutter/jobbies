72877015434a4ead642a9b987adde2c1175a993e
nicolassutter <contact.nicolas.sutter@gmail.com>
2025-01-29 18:03:11 +0100

    feat: add env variables for production

--- packages/app/src/utils/config.ts
+++ packages/app/src/utils/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  authUrl: 'http://localhost:8080',
-  trpcUrl: 'http://localhost:8888/api/trpc',
+  authUrl: import.meta.env.VITE_AUTH_URL ?? 'http://localhost:8080',
+  trpcUrl: import.meta.env.VITE_TRPC_URL ?? 'http://localhost:8888/api/trpc',
 }
