28a2cf7b6cbdebb752b15fb8317d53bbf58068fd
nicolassutter <contact.sutter.nicolas@gmail.com>
2025-01-30 15:27:58 +0100

    feat: continue migration

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,34 @@
 // Import h3 as npm dependency
-import { createApp, createRouter, defineEventHandler, toWebRequest } from 'h3'
+import {
+  createApp,
+  createRouter,
+  defineEventHandler,
+  type H3CorsOptions,
+  handleCors,
+  toWebRequest,
+} from 'h3'
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 import { appRouter } from './trpc/routers/app.router'
 import { createContext } from './trpc/context'
+import { env } from './utils/env'
 
 // Create an app instance
 export const app = createApp()
+
+const cors = defineEventHandler(async (event) => {
+  const corsOptions: H3CorsOptions = {
+    origin: env.DEV ? '*' : ['https://jobbies-app.netlify.app'],
+    credentials: true,
+    preflight: {
+      statusCode: 204,
+    },
+    methods: '*',
+  }
+
+  handleCors(event, corsOptions)
+})
+
+app.use(cors)
 
 // Create a new router and register it in app
 const router = createRouter()
