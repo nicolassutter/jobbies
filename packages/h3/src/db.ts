2d0e61ba89e2faae7c3627eeb5e652ba1f23f500
nicolassutter <contact.sutter.nicolas@gmail.com>
2025-01-30 09:43:44 +0100

    fix: fix ECONNRESET errors with db connection

--- packages/h3/src/db.ts
+++ packages/h3/src/db.ts
@@ -1,7 +1,19 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { env } from './utils/env'
 import * as schema from '../db/schema'
+import pg from 'pg'
 
-export const db = drizzle(env.DATABASE_URL, {
+const { Pool } = pg
+
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+  // this helps with ECONNRESET errors when the db sleeps
+  statement_timeout: 30000, // 30 seconds
+  query_timeout: 30000,
+  connectionTimeoutMillis: 20000, // 20 seconds
+})
+
+export const db = drizzle({
+  client: pool,
   schema,
 })
