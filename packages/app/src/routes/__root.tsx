6384aec1403024324a53f4f7f1dfd8af9eecea24
nicolassutter <contact.sutter.nicolas@gmail.com>
2025-02-01 13:05:19 +0100

    feat: add loader to auth data

--- packages/app/src/routes/__root.tsx
+++ packages/app/src/routes/__root.tsx
@@ -4,16 +4,35 @@ import { createRootRouteWithContext } from '@tanstack/react-router'
 import { AppSidebar } from '~/components/AppSidebar'
 import { SidebarProvider, SidebarTrigger } from '~/components/ui/sidebar'
 import { ensureAuthReady } from '~/stores/session'
+import { SpinLoader } from '~/components/Loaders'
 
 type MyRouterContext = { [key: string]: never }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   beforeLoad: async () => {
     // makes sure the user data (if any) is fetched before rendering the app
-    await ensureAuthReady()
+    const promise = await ensureAuthReady()
+
+    return {
+      promise,
+    }
   },
+  // this is needed because of an issue
+  // https://github.com/TanStack/router/issues/2182#issuecomment-2495539306
+  wrapInSuspense: true,
   component: RootComponent,
+  // according to the docs the pending component will always be shown at least for 500ms
+  // to avoid flashing
+  pendingComponent: PendingComponent,
 })
+
+function PendingComponent() {
+  return (
+    <div className='fixed inset-0 flex items-center justify-center'>
+      <SpinLoader />
+    </div>
+  )
+}
 
 function RootComponent() {
   const loc = useLocation()
