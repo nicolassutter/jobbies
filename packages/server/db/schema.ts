d7dc776882323801afbc9b6e40b4e76e14d6c6b7
nicolassutter <contact.nicolas.sutter@gmail.com>
2025-02-03 18:30:17 +0100

    chore: use a generic function to match database and zod schemas

--- packages/server/db/schema.ts
+++ packages/server/db/schema.ts
@@ -1,30 +1,45 @@
-import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
-import { applicationStatusEnum } from '@internal/shared'
+import {
+  sqliteTable,
+  type SQLiteTableWithColumns,
+  text,
+} from 'drizzle-orm/sqlite-core'
+import { ApplicationSchema, applicationStatusEnum } from '@internal/shared'
 import { randomUUID } from 'node:crypto'
-import { type Application } from '@internal/shared'
-import type { Expect, Equal } from 'type-testing'
 import type { ConditionalExcept, ValueOf, SetOptional } from 'type-fest'
-
-export const applications = sqliteTable('applications', {
-  id: text()
-    .primaryKey()
-    .$defaultFn(() => randomUUID()),
-  jobTitle: text('job_title', { length: 255 }).notNull(),
-  notes: text(),
-  applicationStatus: text('application_status', {
-    length: 255,
-    enum: applicationStatusEnum._def.values,
-  }),
-  url: text({ length: 255 }),
-  userId: text('user_id') // references a Pocketbase user
-    .notNull(),
-})
+import type { z } from 'zod'
 
 type NullableOnly<T> = ConditionalExcept<T, Exclude<ValueOf<T>, null>>
 // nullable keys are set to optional so that zod schemas can use .nullish
 type OptionalNullableKeys<T> = SetOptional<T, keyof NullableOnly<T>>
 
-// TESTS
-type __shoudldMatch = Expect<
-  Equal<Application, OptionalNullableKeys<typeof applications.$inferSelect>>
->
+// drizzle-zod does not work for client-side code for security reasons, at least for now
+// https://github.com/drizzle-team/drizzle-orm/issues/941
+
+/**
+ * This function makes sure that the zod schema extends the table definition.
+ */
+function zodDrizzleTable<
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  TDef extends SQLiteTableWithColumns<any>,
+  TShouldMatch extends z.ZodType<OptionalNullableKeys<TDef['$inferSelect']>>,
+>(tableDef: TDef, _schema: TShouldMatch) {
+  return tableDef
+}
+
+export const applications = zodDrizzleTable(
+  sqliteTable('applications', {
+    id: text()
+      .primaryKey()
+      .$defaultFn(() => randomUUID()),
+    jobTitle: text('job_title', { length: 255 }).notNull(),
+    notes: text(),
+    applicationStatus: text('application_status', {
+      length: 255,
+      enum: applicationStatusEnum._def.values,
+    }),
+    url: text({ length: 255 }),
+    userId: text('user_id') // references a Pocketbase user
+      .notNull(),
+  }),
+  ApplicationSchema,
+)
